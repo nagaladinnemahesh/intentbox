dfc97a6a11aa050b9c625cfd79ad7fa1d36046d4
Mahesh Nagaladinne <maheshnagaladinne@gmail.com>
2025-10-22 16:15:18 +0530

    added elasticSearch,formatted email response

---
+++ backend/src/indexService.ts
@@ -0,0 +1,48 @@
+import { emitWarning } from "process";
+import elasticClient from "./elasticSearchClient.js";
+
+const INDEX_NAME = "emails";
+
+export const createIndexIfNotExists = async () => {
+    const indexExists = await elasticClient.indices.exists({
+        index: INDEX_NAME,
+    })
+    if (!indexExists){
+        await elasticClient.indices.create({
+            index: INDEX_NAME,
+            mappings: {
+                properties: {
+                    from: {type: 'text'},
+                    to: {type: 'text'},
+                    subject: {type: 'text'},
+                    date: {type: 'date'},
+                    snippet: {type: 'text'},
+                    body: {type: 'text'},
+                }
+            }
+        })
+
+        console.log(`Index ${INDEX_NAME} created.`);
+    }
+};
+
+export const indexEmail = async(email: any) => {
+    await elasticClient.index({
+        index: INDEX_NAME,
+        document: email,
+    })
+}
+
+export const searchEmails = async(query: string) => {
+    const result = await elasticClient.search({
+        index: INDEX_NAME,
+        query:{
+            multi_match:{
+                query,
+                fields: ["from", "to", "subject", "snippet", "body"]
+            }
+        }
+    })
+
+    return result.hits.hits.map((hit: any) => hit._source);
+}
